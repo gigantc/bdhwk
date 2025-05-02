21f1f0a327cb20565f00fe7b8c72916d4c3ddb0f
dfree <dan@gigantc.com>
2025-05-02 16:01:17 -0700

    got the profile section locking all nice-like

--- src/containers/Profile/Profile.jsx
+++ src/containers/Profile/Profile.jsx
@@ -9,39 +9,70 @@ import './Profile.scss';
 
 const Profile = () => {
 
-  const profileRef = useRef(null);
-  const textRef = useRef(null);
+  //////////////////////////////////////
+  // REFS
+  const textBlock = useRef(null);
+
+
   
-  
+  //////////////////////////////////////
+  // TEXT LOCKING AND ANIMATIONS
   useEffect(() => {
-    // const timeline = gsap.timeline({
-    //   scrollTrigger: {
-    //     trigger: profileRef.current,
-    //     start: 'top center',
-    //     end: 'bottom center',
-    //     scrub: false,
-    //     pin: true,
-    //     pinSpacing: true,
-    //     markers: true,
-    //   }
-    // });
-  
-    // timeline.fromTo(textRef.current,
-    //   { autoAlpha: 0, y: 40 },
-    //   { autoAlpha: 1, y: 0, duration: 1, ease: 'power2.out' }
-    // );
+
+    const ctx = gsap.context(() => {
+      const textArea = textBlock.current;
+      const h3 = textArea.querySelector('h3');
+
+
+      // Get the height of the h2 element
+      const h3Height = h3.offsetHeight;
+      const h3Offset = -(h3Height)/2;
+      // Set initial translateY in the style of h3
+      // doing this to make sure the text block is centered
+      //gsap.set(h3, { y: -(h3Height - 30)/2 });
+      const timeline = gsap.timeline({
+        scrollTrigger: {
+          trigger: textArea,
+          start: 'top center',
+          end: '+=100%',
+          scrub: true,
+          pin: true,
+          markers: false,
+        },
+      });
+
+      timeline
+        .fromTo(
+          h3,
+          { opacity: 0, filter: 'blur(10px)', y: h3Offset + 50},
+          { opacity: 1, filter: 'blur(0px)', y: (h3Offset), duration: 15 }
+        )
+          // Pause time - 1 second-sih
+        .to({}, { duration: 5 })
+        .to(
+          h3,
+          { 
+            opacity: 0, 
+            filter: 'blur(3px)', 
+            duration: 15 
+          },
+          '+=1'
+        );
+    }, textBlock);
+
+    return () => ctx.revert();
   }, []);
 
-
+  
+  //////////////////////////////////////
+  // RENDER
   return (
     <>
-      <section className="profile" ref={profileRef}>
-        <div className="wrap">
-          <h3 ref={textRef}>Driven by a passion for clean code and seamless digital experiences, I've spent the past 15+ years building fast, scalable websites, custom CMS themes, and ecommerce sites that don't just function—they resonate.    
+      <section className="profile" ref={textBlock}>
+          <h3 >Driven by a passion for clean code and seamless digital experiences, I've spent the past 15+ years building fast, scalable websites, custom CMS themes, and ecommerce sites that don't just function—they resonate.    
           From Fortune 100 giants to scrappy startups, I've helped teams bring ambitious ideas to life through smart architecture, modern JavaScript frameworks, and UX-led design systems. 
           <br /><br />
           Now, I'm focused on what's next—collaborating, experimenting, and pushing the boundaries of what the web can do.</h3>
-        </div>
       </section>
     </>
   );
