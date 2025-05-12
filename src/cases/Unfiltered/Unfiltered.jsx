850dbd7e81668e86b90cef07358f9f1bb3d5922c
dfree <dan@gigantc.com>
2025-05-12 14:07:36 -0700

    case study structure

--- src/cases/Unfiltered/Unfiltered.jsx
+++ src/cases/Unfiltered/Unfiltered.jsx
@@ -5,6 +5,16 @@ import { gsap } from 'gsap';
 import './Unfiltered.scss';
 
 
+import Hero from './assets/hero.jpg'
+import Gallery1 from './assets/gallery-table.jpg'
+import Gallery2 from './assets/gallery-frame.jpg'
+import Gallery3 from './assets/gallery-frame-2.jpg'
+import Gallery4 from './assets/gallery-timeline.jpg'
+import Gallery5 from './assets/gallery-ai.jpg'
+import Gallery6 from './assets/gallery-chair.jpg'
+import Gallery7 from './assets/gallery-exhibit.jpg'
+
+import LinkIcon from '../../assets/link-arrow-up.svg?react';
 
 const Unfiltered = () => {
 
@@ -107,7 +117,7 @@ const Unfiltered = () => {
   // RENDER
   return (
     <section className="case-study">
-      
+
       {showSplash && (
         <div className="splash-screen" ref={SplashRef}>
           <div className="intro-text">
@@ -121,16 +131,82 @@ const Unfiltered = () => {
       {!showSplash && (
         <section className="unfiltered" ref={UnfilteredRef}>
           <div className="wrap">
-            <h1>Unfiltered</h1>
-            <p>Big Tobacco's Exploitation of Black America</p>
 
-            <p>year 2025</p>
-            <p>client The State of Utah</p>
-            <p>design Destin Lastname, Ryan Lord</p>
-            <p>stack Vite/React, GSAP, Sass, Three.js</p>
-            <p>View Live Site</p>
+            <div className="title">
+              <h1>Unfiltered</h1>
+              <h3>A Scrollytelling, Digital Art Installation</h3>
+            </div>
 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div className="image">
+              <img src={Hero}npm  alt="Unfiltered Hero Screenshot" />
+            </div>
+            
+
+            <div className="body">
+              
+              <div className="writeup">
+                <h2>Dismantling a system purposefully built on smoke and mirrors.</h2>
+
+                <p>The State of Utah tasked R&R Partners with creating a temporary art installation to expose Big Tobacco's legacy of manipulative, targeted advertising. The result was a visceral, immersive exhibit called “Unfiltered." It was a huge success. It confronted visitors with walls of original ads, old magazine spreads, and TV commercials that showcased the industry's aggressive tactics in communities of color.</p>
+
+                <p><strong>But once the installation came down, we didn't want the message to disappear with it.</strong></p>
+
+                <p>With no budget and a tight window, we set out to reimagine the installation digitally. The concept was simple, recreate the sensory overload of the original installation through a "scrollytelling" experience. Users are pulled through a hijacked scroll narrative that mimics the exhibit's multi-room experience.</p>
+
+                <p>The build was quick. <strong>Just eight days of development.</strong> I had to prioritize ruthlessly, move fast, and manage a ton of stress. There were many compromises, but the end result still punches above its weight as a standalone digital experience.</p>
+
+                <p>Unfiltered now lives on as a digital story that doesn't just preserve the exhibit, it reframes it. Even with zero budget the site captures the spirit of the original and extends its reach far beyond gallery walls.</p>
+
+                <p>This one was so much fun to build, and I'm proud of what came together in such a short time.</p>
+
+              </div>
+
+              <div className="info">
+                <div className="box">
+                  <p><span>Year:</span> 2025</p>
+                  <p><span>Client:</span>The State of Utah</p>
+                  <p><span>Design:</span>Destin Cox, Ryan Lord</p>
+                  <p><span>Timeline:</span>less than 2 weeks (8 dev days)</p>
+                  <p><span>Budget:</span>$0.00</p>
+                  <p><span>Stack:</span>Vite/React, GSAP, Sass, Three.js</p>
+                </div>
+
+                <a href="https://theunfilteredstory.org" target="_blank"><LinkIcon /> View Live Site</a>
+              </div>
+
+            </div>
+
+
+
+
+            <div className="gallery">
+              <div className="card">
+                <img src={Gallery1} alt="" />
+              </div>
+              <div className="card">
+                <img src={Gallery2} alt="" />
+              </div>
+              <div className="card">
+                <img src={Gallery3} alt="" />
+              </div>
+              <div className="card">
+                <img src={Gallery7} alt="" />
+              </div>
+               <div className="card">
+                <img src={Gallery5} alt="" />
+              </div>
+               <div className="card">
+                <img src={Gallery6} alt="" />
+              </div>
+               <div className="card">
+                <img src={Gallery4} alt="" />
+              </div>
+            </div>
+            
+            
+
+            
+            
           </div>
         </section>
       )}
