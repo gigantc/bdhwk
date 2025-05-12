44137d03e3e06d4e0d60ea2486ab50263ffd8862
dfree <dan@gigantc.com>
2025-05-12 08:42:05 -0700

    setting up the case studies

--- src/cases/Unfiltered/Unfiltered.jsx
+++ src/cases/Unfiltered/Unfiltered.jsx
@@ -8,11 +8,13 @@ import './Unfiltered.scss';
 
 const Unfiltered = () => {
 
+
   //////////////////////////////////////
   // REFS & STATE
   const [showSplash, setShowSplash] = useState(true);
   const SplashRef = useRef(null);
   const TitleRef = useRef(null);
+  const UnfilteredRef = useRef(null);
 
 
   //////////////////////////////////////
@@ -23,7 +25,6 @@ const Unfiltered = () => {
     const title = TitleRef.current;
 
     const tl = gsap.timeline({ onComplete: () => setShowSplash(false) });
-
 
     if (splash) {
 
@@ -78,10 +79,35 @@ const Unfiltered = () => {
 
 
 
+
+  // Animate .unfiltered when splash is gone
+  useEffect(() => {
+    if (!showSplash && UnfilteredRef.current) {
+      gsap.fromTo(
+        UnfilteredRef.current,
+        {
+          opacity: 0,
+          y: 100,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          duration: 1,
+          ease: 'power2.out'
+        }
+      );
+    }
+  }, [showSplash]);
+
+  
+
+
+
   //////////////////////////////////////
   // RENDER
   return (
     <section className="case-study">
+      
       {showSplash && (
         <div className="splash-screen" ref={SplashRef}>
           <div className="intro-text">
@@ -93,7 +119,7 @@ const Unfiltered = () => {
       )}
 
       {!showSplash && (
-        <section className="unfiltered">
+        <section className="unfiltered" ref={UnfilteredRef}>
           <div className="wrap">
             <h1>Unfiltered</h1>
             <p>Big Tobacco's Exploitation of Black America</p>
@@ -102,6 +128,7 @@ const Unfiltered = () => {
             <p>client The State of Utah</p>
             <p>design Destin Lastname, Ryan Lord</p>
             <p>stack Vite/React, GSAP, Sass, Three.js</p>
+            <p>View Live Site</p>
 
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
