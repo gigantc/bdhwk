ac45e5e4f609faa1e0049ed64c4741a016bfc06c
dfree <dan@gigantc.com>
2025-05-14 07:55:47 -0700

    moveing firebase to .env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyATo3Z-GwHAUC4vrN51ZC0SSAgoubDzLlE",
-  authDomain: "bdhwk-5daf0.firebaseapp.com",
-  projectId: "bdhwk-5daf0",
-  storageBucket: "bdhwk-5daf0.firebasestorage.app",
-  messagingSenderId: "361533796037",
-  appId: "1:361533796037:web:e4288ba3644b75bab80aac"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
